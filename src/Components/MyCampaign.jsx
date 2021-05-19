bf29e753090861e96f91e4faaefd23569cfc5fbe
Yagel <yageljaoui@gmail.com>
2021-05-19 13:22:18 +0300

    good

--- src/Components/MyCampaign.jsx
+++ src/Components/MyCampaign.jsx
@@ -5,49 +5,185 @@ export default function MyCampaign() {
 	return (
 		<div className='container'>
 			<h2>My Campaign</h2>
-			<div className='stages'>
-				<div className='tooltip'>ToolTip Text</div>
-				Planning
-			</div>
-			<img className='arrowRight' src={ArrowRight} alt='' />
-			<div className='stages'>
-				<div className='tooltip'>ToolTip Text</div>
-				Planning
-			</div>
-			<img className='arrowRight' src={ArrowRight} alt='' />
-			<div className='stages'>
-				<div className='tooltip'>ToolTip Text</div>
-				Planning
-			</div>
-			<img className='arrowRight' src={ArrowRight} alt='' />
-			<div className='stages'>
-				<div className='tooltip'>ToolTip Text</div>
-				Planning
-			</div>
-			<img className='arrowRight' src={ArrowRight} alt='' />
-			<div className='stages'>
-				<div className='tooltip'>ToolTip Text</div>
-				Planning
-			</div>
-			<img className='arrowRight' src={ArrowRight} alt='' />
-			<div className='stages'>
-				<div className='tooltip'>ToolTip Text</div>
-				Planning
-			</div>
-			<img className='arrowRight' src={ArrowRight} alt='' />
-			<div className='stages'>
-				<div className='tooltip'>ToolTip Text</div>
-				Planning
-			</div>
-			<img className='arrowRight' src={ArrowRight} alt='' />
-			<div className='stages'>
-				<div className='tooltip'>ToolTip Text</div>
-				Planning
-			</div>
-			<img className='arrowRight' src={ArrowRight} alt='' />
-			<div className='stages'>
-				<div className='tooltip'>ToolTip Text</div>
-				Planning
+			<div className='navbar'>
+				<div className='subnav'>
+					<button className='subnavbtn'>
+						Panning <i className='fa fa-caret-down'></i>
+					</button>
+					<img className='arrowRight' src={ArrowRight} alt='' />
+					<div className='subnav-content'>
+						<a className='subnav-item' href='#company'>
+							management
+						</a>
+						<a className='subnav-item' href='#team'>
+							Team
+						</a>
+						<a className='subnav-item' href='#careers'>
+							Careers
+						</a>
+					</div>
+				</div>
+				<div className='subnav'>
+					<button className='subnavbtn'>
+						Servies <i className='fa fa-caret-down'></i>
+					</button>
+					<img className='arrowRight' src={ArrowRight} alt='' />
+					<div className='subnav-content'>
+						<a className='subnav-item' href='#bring'>
+							Bring
+						</a>
+						<a className='subnav-item' href='#deliver'>
+							Deliver
+						</a>
+						<a className='subnav-item' href='#package'>
+							Package
+						</a>
+						<a className='subnav-item' href='#express'>
+							Express
+						</a>
+					</div>
+				</div>
+				<div className='subnav'>
+					<button className='subnavbtn'>
+						Servies <i className='fa fa-caret-down'></i>
+					</button>
+					<img className='arrowRight' src={ArrowRight} alt='' />
+					<div className='subnav-content'>
+						<a className='subnav-item' href='#bring'>
+							Bring
+						</a>
+						<a className='subnav-item' href='#deliver'>
+							Deliver
+						</a>
+						<a className='subnav-item' href='#package'>
+							Package
+						</a>
+						<a className='subnav-item' href='#express'>
+							Express
+						</a>
+					</div>
+				</div>
+				<div className='subnav'>
+					<button className='subnavbtn'>
+						Servies <i className='fa fa-caret-down'></i>
+					</button>
+					<img className='arrowRight' src={ArrowRight} alt='' />
+					<div className='subnav-content'>
+						<a className='subnav-item' href='#bring'>
+							Bring
+						</a>
+						<a className='subnav-item' href='#deliver'>
+							Deliver
+						</a>
+						<a className='subnav-item' href='#package'>
+							Package
+						</a>
+						<a className='subnav-item' href='#express'>
+							Express
+						</a>
+					</div>
+				</div>
+				<div className='subnav'>
+					<button className='subnavbtn'>
+						Servies <i className='fa fa-caret-down'></i>
+					</button>
+					<img className='arrowRight' src={ArrowRight} alt='' />
+					<div className='subnav-content'>
+						<a className='subnav-item' href='#bring'>
+							Bring
+						</a>
+						<a className='subnav-item' href='#deliver'>
+							Deliver
+						</a>
+						<a className='subnav-item' href='#package'>
+							Package
+						</a>
+						<a className='subnav-item' href='#express'>
+							Express
+						</a>
+					</div>
+				</div>
+				<div className='subnav'>
+					<button className='subnavbtn'>
+						Servies <i className='fa fa-caret-down'></i>
+					</button>
+					<img className='arrowRight' src={ArrowRight} alt='' />
+					<div className='subnav-content'>
+						<a className='subnav-item' href='#bring'>
+							Bring
+						</a>
+						<a className='subnav-item' href='#deliver'>
+							Deliver
+						</a>
+						<a className='subnav-item' href='#package'>
+							Package
+						</a>
+						<a className='subnav-item' href='#express'>
+							Express
+						</a>
+					</div>
+				</div>
+				<div className='subnav'>
+					<button className='subnavbtn'>
+						Servies <i className='fa fa-caret-down'></i>
+					</button>
+					<img className='arrowRight' src={ArrowRight} alt='' />
+					<div className='subnav-content'>
+						<a className='subnav-item' href='#bring'>
+							Bring
+						</a>
+						<a className='subnav-item' href='#deliver'>
+							Deliver
+						</a>
+						<a className='subnav-item' href='#package'>
+							Package
+						</a>
+						<a className='subnav-item' href='#express'>
+							Express
+						</a>
+					</div>
+				</div>
+				<div className='subnav'>
+					<button className='subnavbtn'>
+						Servies <i className='fa fa-caret-down'></i>
+					</button>
+					<img className='arrowRight' src={ArrowRight} alt='' />
+					<div className='subnav-content'>
+						<a className='subnav-item' href='#bring'>
+							Bring
+						</a>
+						<a className='subnav-item' href='#deliver'>
+							Deliver
+						</a>
+						<a className='subnav-item' href='#package'>
+							Package
+						</a>
+						<a className='subnav-item' href='#express'>
+							Express
+						</a>
+					</div>
+				</div>
+				<div className='subnav'>
+					<button className='subnavbtn'>
+						<div className='tooltip'>ToolTip Text</div>
+						Partners <i className='fa fa-caret-down'></i>
+					</button>
+
+					<div className='subnav-content'>
+						<a className='subnav-item' href='#link1'>
+							Link 1
+						</a>
+						<a className='subnav-item' href='#link2'>
+							Link 2
+						</a>
+						<a className='subnav-item' href='#link3'>
+							Link 3
+						</a>
+						<a className='subnav-item' href='#link4'>
+							Link 4
+						</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
